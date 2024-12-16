00127f9b7afadf75c14476a074e796c1379ef57f
ExternalAddress4401 <external_address@hotmail.com>
2024-12-15 19:41:12 -0800

    Modify imports

--- index.ts
+++ index.ts
@@ -4,12 +4,16 @@ import { proto as LangConfig } from "./protos/LangConfig";
 import { proto as ScalingConfig } from "./protos/ScalingConfig";
 import { proto as SongConfig } from "./protos/SongConfig";
 import { getCms } from "./server/CMSRequester";
+import { ProtobufReader } from "./ProtobufReader";
+import { ProtobufWriter } from "./ProtobufWriter";
 
-export default {
+export {
   AssetsPatchConfigProto,
   FontFallbackConfig,
   LangConfig,
   ScalingConfig,
   SongConfig,
+  ProtobufReader,
+  ProtobufWriter,
   getCms,
 };
